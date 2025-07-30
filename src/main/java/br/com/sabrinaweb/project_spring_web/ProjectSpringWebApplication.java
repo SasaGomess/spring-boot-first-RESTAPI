3b94539b2927c8c44dd6712d05953cd83d0af937
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-07-30 18:54:44 -0300

    feat: project created

---
+++ src/main/java/br/com/sabrinaweb/project_spring_web/ProjectSpringWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.sabrinaweb.project_spring_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sabrinaweb/project_spring_web/ProjectSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sabrinaweb.project_spring_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
